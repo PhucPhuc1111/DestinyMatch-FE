247fb184af401b651a0ee7f06e5acc0f2b5b6486
NET05_TrinhSonTung <tungtsse172875@fpt.edu.vn>
2024-05-21 16:10:49 +0700

    update

--- DM-BE/DataAccess/Models/DestinyMatchContext.cs
+++ DM-BE/DataAccess/Models/DestinyMatchContext.cs
@@ -41,10 +41,6 @@ public partial class DestinyMatchContext : DbContext
 
     public virtual DbSet<University> Universities { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=DestinyMatch;uid=sa;pwd=12345;TrustServerCertificate=True");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Account>(entity =>
